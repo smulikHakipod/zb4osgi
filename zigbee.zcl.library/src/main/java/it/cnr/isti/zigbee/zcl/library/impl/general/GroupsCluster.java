4350bd9b5203175ade5ed56b654e57bfc9643c6f
stefano.lenzi <stefano.lenzi@a5b69b06-b4b2-4dab-a1bb-7adf784d9d5f>
2013-08-06 13:33:08 +0000

    Removed unused code

--- zigbee.zcl.library/src/main/java/it/cnr/isti/zigbee/zcl/library/impl/general/GroupsCluster.java
+++ zigbee.zcl.library/src/main/java/it/cnr/isti/zigbee/zcl/library/impl/general/GroupsCluster.java
@@ -1,10 +1,10 @@
 /*
    Copyright 2008-2010 CNR-ISTI, http://isti.cnr.it
-   Institute of Information Science and Technologies 
-   of the Italian National Research Council 
+   Institute of Information Science and Technologies
+   of the Italian National Research Council
 
 
-   See the NOTICE file distributed with this work for additional 
+   See the NOTICE file distributed with this work for additional
    information regarding copyright ownership
 
    Licensed under the Apache License, Version 2.0 (the "License");
@@ -28,7 +28,6 @@ import it.cnr.isti.zigbee.zcl.library.api.core.Response;
 import it.cnr.isti.zigbee.zcl.library.api.core.ZigBeeClusterException;
 import it.cnr.isti.zigbee.zcl.library.api.general.Groups;
 import it.cnr.isti.zigbee.zcl.library.impl.attribute.Attributes;
-import it.cnr.isti.zigbee.zcl.library.impl.core.AbstractCommand;
 import it.cnr.isti.zigbee.zcl.library.impl.core.AttributeImpl;
 import it.cnr.isti.zigbee.zcl.library.impl.core.EmptyPayloadCommand;
 import it.cnr.isti.zigbee.zcl.library.impl.core.ZCLClusterBase;
@@ -43,7 +42,7 @@ import it.cnr.isti.zigbee.zcl.library.impl.general.groups.ViewGroupCommand;
 import it.cnr.isti.zigbee.zcl.library.impl.general.groups.ViewGroupResponseImpl;
 import it.cnr.isti.zigbee.zcl.library.impl.global.DefaultResponseImpl;
 /**
- * 
+ *
  * @author <a href="mailto:stefano.lenzi@isti.cnr.it">Stefano "Kismet" Lenzi</a>
  * @author <a href="mailto:francesco.furfari@isti.cnr.it">Francesco Furfari</a>
  * @version $LastChangedRevision$ ($LastChangedDate$)
@@ -51,79 +50,79 @@ import it.cnr.isti.zigbee.zcl.library.impl.global.DefaultResponseImpl;
  */
 public class GroupsCluster extends ZCLClusterBase implements Groups {
 
-	private final AttributeImpl nameSupport;
-	
-	private final Attribute[] attributes;
-		
-	private static EmptyPayloadCommand CMD_REMOVE_ALL_GROUP = new EmptyPayloadCommand()
-	.setId(Groups.REMOVE_ALL_GROUP_ID)
-	.setClientServerDirection(true)
-	.setClusterSpecific(true)
-	.setManufacturerExtension(false);
-	
-	
-	public GroupsCluster(ZigBeeDevice zbDevice){
-		super(zbDevice);
-		nameSupport = new AttributeImpl(zbDevice,this,Attributes.NAME_SUPPORT_GROUPS);
-		attributes = new AttributeImpl[]{nameSupport};
-	}
-	
-	@Override
-	public short getId() {
-		return Groups.ID;
-	}
+    private final AttributeImpl nameSupport;
 
-	@Override
-	public String getName() {
-		return Groups.NAME;
-	}
+    private final Attribute[] attributes;
 
-	@Override
-	public Attribute[] getStandardAttributes() {
-		return attributes;
-	}
+    private static EmptyPayloadCommand CMD_REMOVE_ALL_GROUP = new EmptyPayloadCommand()
+    .setId(Groups.REMOVE_ALL_GROUP_ID)
+    .setClientServerDirection(true)
+    .setClusterSpecific(true)
+    .setManufacturerExtension(false);
 
-	public Response addGroup(int groupId, String name) throws ZigBeeClusterException {
-		enableDefaultResponse();
-		AddGroupCommand addGroupCmd = new AddGroupCommand(groupId, name);
-		Response response = invoke(addGroupCmd);
-		return  new AddGroupResponseImpl(response);
-	}
 
-	public Response addGroupIfIdentifying(int groupId, String name) throws ZigBeeClusterException {
-		enableDefaultResponse();
-		AddGroupIfIdentyfingCommand addGroupIfIdCMD = new AddGroupIfIdentyfingCommand(groupId,name);
-		Response response = invoke(addGroupIfIdCMD);
-		return new DefaultResponseImpl(response);
-	}
+    public GroupsCluster(ZigBeeDevice zbDevice){
+        super(zbDevice);
+        nameSupport = new AttributeImpl(zbDevice,this,Attributes.NAME_SUPPORT_GROUPS);
+        attributes = new AttributeImpl[]{nameSupport};
+    }
 
-	public Attribute getAttributeNameSupport() {
-		return nameSupport;
-	}
+    @Override
+    public short getId() {
+        return Groups.ID;
+    }
 
-	public Response getGroupMembership(int[] groupList) throws ZigBeeClusterException {
-		enableDefaultResponse();
-		GetGroupMembershipCommand getGroupMemCmd = new GetGroupMembershipCommand(groupList);
-		Response response = invoke(getGroupMemCmd); 
-		return  new GetGroupMembershipResponseImpl(response);
-	}
+    @Override
+    public String getName() {
+        return Groups.NAME;
+    }
 
-	public Response removeAllGroup() throws ZigBeeClusterException{
-		enableDefaultResponse();
-		Response response = invoke(CMD_REMOVE_ALL_GROUP); 
-		return  new DefaultResponseImpl(response);
-	}
+    @Override
+    public Attribute[] getStandardAttributes() {
+        return attributes;
+    }
 
-	public Response removeGroup(int groupId) throws ZigBeeClusterException{
-		RemoveGroupCommand removeGroupCmd = new RemoveGroupCommand(groupId);
-		Response response = invoke(removeGroupCmd);
-		return new RemoveGroupResponseImpl(response);
-	}
+    public Response addGroup(int groupId, String name) throws ZigBeeClusterException {
+        enableDefaultResponse();
+        AddGroupCommand addGroupCmd = new AddGroupCommand(groupId, name);
+        Response response = invoke(addGroupCmd);
+        return  new AddGroupResponseImpl(response);
+    }
 
-	public Response viewGroup(int groupId) throws ZigBeeClusterException{
-		ViewGroupCommand viewGroupCmd = new ViewGroupCommand(groupId);
-		Response response = invoke(viewGroupCmd);
-		return new ViewGroupResponseImpl(response);
-	}
+    public Response addGroupIfIdentifying(int groupId, String name) throws ZigBeeClusterException {
+        enableDefaultResponse();
+        AddGroupIfIdentyfingCommand addGroupIfIdCMD = new AddGroupIfIdentyfingCommand(groupId,name);
+        Response response = invoke(addGroupIfIdCMD);
+        return new DefaultResponseImpl(response);
+    }
+
+    public Attribute getAttributeNameSupport() {
+        return nameSupport;
+    }
+
+    public Response getGroupMembership(int[] groupList) throws ZigBeeClusterException {
+        enableDefaultResponse();
+        GetGroupMembershipCommand getGroupMemCmd = new GetGroupMembershipCommand(groupList);
+        Response response = invoke(getGroupMemCmd);
+        return  new GetGroupMembershipResponseImpl(response);
+    }
+
+    public Response removeAllGroup() throws ZigBeeClusterException{
+        enableDefaultResponse();
+        Response response = invoke(CMD_REMOVE_ALL_GROUP);
+        return  new DefaultResponseImpl(response);
+    }
+
+    public Response removeGroup(int groupId) throws ZigBeeClusterException{
+        RemoveGroupCommand removeGroupCmd = new RemoveGroupCommand(groupId);
+        Response response = invoke(removeGroupCmd);
+        return new RemoveGroupResponseImpl(response);
+    }
+
+    public Response viewGroup(int groupId) throws ZigBeeClusterException{
+        ViewGroupCommand viewGroupCmd = new ViewGroupCommand(groupId);
+        Response response = invoke(viewGroupCmd);
+        return new ViewGroupResponseImpl(response);
+    }
 
 }
