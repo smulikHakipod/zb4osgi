eb38234955c1e53e3816a5fe2c773d929bf62ce6
stefano.lenzi <stefano.lenzi@a5b69b06-b4b2-4dab-a1bb-7adf784d9d5f>
2014-07-11 11:59:49 +0000

    Adding Javadoc a better output message ( refs #251 )

--- zigbee.dongle.api/src/test/java/it/cnr/isti/zigbee/dongle/api/DriverStatusTest.java
+++ zigbee.dongle.api/src/test/java/it/cnr/isti/zigbee/dongle/api/DriverStatusTest.java
@@ -4,16 +4,29 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+/**
+ *
+ *
+ * @author <a href="mailto:stefano.lenzi@isti.cnr.it">Stefano "Kismet" Lenzi -
+ *         ISTI-CNR
+ * @version $LastChangedRevision$ ($LastChangedDate$)
+ * @since 0.8.0
+ *
+ */
 public class DriverStatusTest {
 
     @Test
     public void testDriverStatusEnum() {
-        String[] expectedStatus = {"CREATED", "HARDWARE_INITIALIZING", "HARDWARE_READY", "NETWORK_INITIALIZING", "NETWORK_READY", "CLOSED"};
+        final String WARNING = "please be aware that adding or modifying states has side effects on implementation";
+
+        String[] expectedStatus = {
+                "CREATED", "HARDWARE_INITIALIZING", "HARDWARE_READY", "NETWORK_INITIALIZING", "NETWORK_READY", "CLOSED"
+        };
         DriverStatus[] status = DriverStatus.values();
         for (int i = 0; i < status.length; i++) {
             DriverStatus match = DriverStatus.valueOf(expectedStatus[i]);
-            assertEquals("Expected status name ", match, status[i]);
-            assertEquals("Expected status ordinal ", i, match.ordinal());
+            assertEquals("Expected status name error, " + WARNING, match, status[i]);
+            assertEquals("Expected status ordinal error, " + WARNING, i, match.ordinal());
         }
     }
 
