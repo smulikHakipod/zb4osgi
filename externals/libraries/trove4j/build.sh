de8438b44c0dcef956d48b15e70c851db25f7b28
giancarlo.riolo <giancarlo.riolo@a5b69b06-b4b2-4dab-a1bb-7adf784d9d5f>
2014-12-11 13:44:37 +0000

    cleaning trove4j

--- externals/libraries/trove4j/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-ARTIFACT=trove
-
-mvn install:install-file -DpomFile=pom.xml -Dfile=${ARTIFACT}.jar 
-mvn install:install-file -DpomFile=pom.xml -Dfile=${ARTIFACT}-sources.jar -Dclassifier=sources
-mvn install:install-file -DpomFile=pom.xml -Dfile=${ARTIFACT}-javadocs.jar -Dclassifier=javadocs
